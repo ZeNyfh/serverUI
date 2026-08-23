4ee00c34a71d95ec161c00f7ba8e167dde386997
Dawid Barka <psydb14@nottingham.ac.uk>
2026-08-23 19:44:44 +0100

    ensure main starts db and api reg

--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("frontend/css"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("frontend/js"))))
-	http.Handle("/", http.FileServer(http.Dir("frontend/html")))
+	if err := os.MkdirAll("data", 0755); err != nil {
+		log.Fatal(err)
+	}
 
-	log.Println("Serving http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	users, err := openUserStore("data/users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer users.close()
+	allowed, err := loadAllowedUsers("config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	app := &app{users: users, allowed: allowed, sessions: make(map[string]int64)}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/users", users.createUserHandler)
+	mux.HandleFunc("POST /api/login", app.loginHandler)
+	mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("frontend/css"))))
+	mux.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("frontend/js"))))
+	mux.HandleFunc("/", app.rootHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Serving http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
