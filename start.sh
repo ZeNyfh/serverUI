155ab6c7d2dcfebee46b1cc414f6827ea99f7637
Dawid Barka <psydb14@nottingham.ac.uk>
2026-08-24 04:04:21 +0100

    make tailscale start in the bg

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ command -v tailscale >/dev/null 2>&1 || {
 }
 
 tailscale up
-tailscale serve --https=443 http://127.0.0.1:8080
-tailscale serve --https=2283 http://127.0.0.1:2283
+tailscale serve --bg --https=443 http://127.0.0.1:8080
+tailscale serve --bg --https=2283 http://127.0.0.1:2283
 
 go build -o server ./backend
 exec ./server
