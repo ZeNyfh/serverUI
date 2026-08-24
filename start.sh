99d45f0345d3a62d458085f250364834212c11ca
Dawid Barka <psydb14@nottingham.ac.uk>
2026-08-24 04:21:06 +0100

    add beszel to tailscale

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ command -v tailscale >/dev/null 2>&1 || {
 tailscale up
 tailscale serve --bg --https=443 http://127.0.0.1:8080
 tailscale serve --bg --https=2283 http://127.0.0.1:2283
+tailscale serve --bg --https=8090 http://127.0.0.1:8090
 
 go build -o server ./backend
 exec ./server
