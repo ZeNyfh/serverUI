a6d274378dbf0e419d241581942fe1b738a12bc2
Dawid Barka <psydb14@nottingham.ac.uk>
2026-08-24 03:38:12 +0100

    add tailscale to start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,13 @@
 
 set -e
 
+command -v tailscale >/dev/null 2>&1 || {
+  echo "tailscale is required; see README.md for installation instructions." >&2
+  exit 1
+}
+
+tailscale up
+tailscale serve --https=443 http://127.0.0.1:8080
+
 go build -o server ./backend
 exec ./server
